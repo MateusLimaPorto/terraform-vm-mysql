cb808a7ccfaf58f4281be3750aaa791b73429453
Cristiano Santos <cris91@hotmail.com.br>
2021-10-20 22:21:12 -0300

    inserindo script de criação de usuario no mysql

--- mysql-azure.tf
+++ mysql-azure.tf
@@ -185,3 +185,40 @@ resource "time_sleep" "wait_30_seconds_db" {
   depends_on = [azurerm_linux_virtual_machine.vm_mysql]
   create_duration = "30s"
 }
+
+resource "null_resource" "upload_db" {
+    provisioner "file" {
+        connection {
+            type = "ssh"
+            user = var.user
+            password = var.password
+            host = data.azurerm_public_ip.ip_aula_data_db.ip_address
+        }
+        source = "mysql"
+        destination = "/home/azureuser"
+    }
+
+    depends_on = [ time_sleep.wait_30_seconds_db ]
+}
+
+resource "null_resource" "deploy_db" {
+    triggers = {
+        order = null_resource.upload_db.id
+    }
+    provisioner "remote-exec" {
+        connection {
+            type = "ssh"
+            user = var.user
+            password = var.password
+            host = data.azurerm_public_ip.ip_aula_data_db.ip_address
+        }
+        inline = [
+            "sudo apt-get update",
+            "sudo apt-get install -y mysql-server-5.7",
+            "sudo mysql < /home/azureuser/mysql/script/user.sql",
+            "sudo cp -f /home/azureuser/mysql/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf",
+            "sudo service mysql restart",
+            "sleep 20",
+        ]
+    }
+}
